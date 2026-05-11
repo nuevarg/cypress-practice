516c255010d4ae1f7cc3ea73049b38f3811d6c39
Rifqi <vampvarg@live.com>
2026-05-11 17:07:15 +0700

    update reporting config

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,25 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  video: false,
+  //failOnStatusCode: false,
+  chromeWebSecurity: false,
+  fixturesFolder: "./cypress/fixtures",
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    saveJson: true,
+    charts: true,
+    reportPageTitle: "cyrpress-practice",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    videoOnFailOnly: false,
+    ignoreVideos: false,
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
