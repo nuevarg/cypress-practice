16176949c26fc22af6dc171796cc01748decbd47
Rifqi <vampvarg@live.com>
2026-05-13 10:51:43 +0700

    replace logStep with step

--- cypress/e2e/ui_login.cy.js
+++ cypress/e2e/ui_login.cy.js
@@ -5,55 +5,55 @@ import InventoryPage from "../page/inventoryPage.js";
 
 context("Visit all pages", () => {
   beforeEach(() => {
-    cy.allure().logStep("Open SauceDemo website");
+    cy.allure().step("Open SauceDemo website");
     cy.visit("/");
   });
 
   it("should visit login page", () => {
-    cy.allure().logStep("Verify username field is visible");
+    cy.allure().step("Verify username field is visible");
     cy.get(LoginPage.usernameField).should("be.visible");
-    cy.allure().logStep("Verify password field is visible");
+    cy.allure().step("Verify password field is visible");
     cy.get(LoginPage.passwordField).should("be.visible");
-    cy.allure().logStep("Verify login button is visible");
+    cy.allure().step("Verify login button is visible");
     cy.get(LoginPage.loginButton).should("be.visible");
-    cy.allure().logStep("Verify login credentials are visible");
+    cy.allure().step("Verify login credentials are visible");
     cy.get(LoginPage.loginCredentials).should("be.visible");
-    cy.allure().logStep("Verify login password is visible");
+    cy.allure().step("Verify login password is visible");
     cy.get(LoginPage.loginPassword).should("be.visible");
   });
 
   it("should be able to login without errors", () => {
-    cy.allure().logStep("Login with valid credentials");
+    cy.allure().step("Login with valid credentials");
     cy.login();
-    cy.allure().logStep("Verify user is redirected to inventory page");
+    cy.allure().step("Verify user is redirected to inventory page");
     cy.url().should("include", "inventory.html");
-    cy.allure().logStep("Verify inventory title is visible");
+    cy.allure().step("Verify inventory title is visible");
     cy.get(InventoryPage.inventoryTitle).should("be.visible");
   });
 
   it("should be able to logout without errors", () => {
-    cy.allure().logStep("Login with valid credentials");
+    cy.allure().step("Login with valid credentials");
     cy.login();
-    cy.allure().logStep("Logout");
+    cy.allure().step("Logout");
     cy.logout();
-    cy.allure().logStep("Verify user is redirected to login page");
+    cy.allure().step("Verify user is redirected to login page");
     cy.url().should("eq", "https://www.saucedemo.com/");
-    cy.allure().logStep("Verify username field is visible");
+    cy.allure().step("Verify username field is visible");
     cy.get(LoginPage.usernameField).should("be.visible");
-    cy.allure().logStep("Verify password field is visible");
+    cy.allure().step("Verify password field is visible");
     cy.get(LoginPage.passwordField).should("be.visible");
-    cy.allure().logStep("Verify login button is visible");
+    cy.allure().step("Verify login button is visible");
     cy.get(LoginPage.loginButton).should("be.visible");
   });
 
   it("should not be able to login with random credentials", () => {
-    cy.allure().logStep("Enter random username");
+    cy.allure().step("Enter random username");
     cy.get(LoginPage.usernameField).type("randomuser");
-    cy.allure().logStep("Enter random password");
+    cy.allure().step("Enter random password");
     cy.get(LoginPage.passwordField).type("randompassword");
-    cy.allure().logStep("Click login button");
+    cy.allure().step("Click login button");
     cy.get(LoginPage.loginButton).click();
-    cy.allure().logStep("Verify error message is visible");
+    cy.allure().step("Verify error message is visible");
     cy.get(LoginPage.errorMessage).should("be.visible");
   });
 });
